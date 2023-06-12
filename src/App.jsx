024557b8049c6479e843cbea6b11407f0af59413
musta junior <mustafaenfadal@gmail.com>
2023-06-12 23:59:05 +0100

    textovideos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import ListaCard from "./components/ListVideos";
+import Card from "./components/CardVideos"
 
 function App() {
-  return <ListaCard />;
+  return (
+    <>
+    <ListaCard />
+    <Card />
+    </>
+  )
+
 }
 
 export default App;
