6ed33a033b4107061e54617a42037ef4830214b3
musta junior <mustafaenfadal@gmail.com>
2023-06-14 21:59:25 +0100

    nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import './NavBar.css'
+import logo from '../assets/logocanarias.png'
 function NavBar() {
   return (
-    <p style={{color: '#024D71'}}>WelcomeCanarias</p>
+    <header className="header">
+    <div className="containt-header">
+    <p className='title-header'>WelcomeCanarias</p>
+    <img className="logo-header" src={logo}/>
+    </div>
+    </header>
   )
 }
+
+
 
 export default NavBar
