86295ac503f9da5a931f79ac646bb7eb3e22fb0c
musta junior <mustafaenfadal@gmail.com>
2023-06-14 21:35:25 +0100

    headernav

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import ListaCard from "./components/ListVideos";
 import Card from "./components/CardVideos"
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+    <NavBar />
     <ListaCard />
     <Card />
     </>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <p style={{color: '#024D71'}}>WelcomeCanarias</p>
+  )
+}
+
+export default NavBar
